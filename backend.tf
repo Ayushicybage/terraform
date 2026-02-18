cabed4d9f7547fc58cb28d48d857f3f4bfeaf89a
Ayushi Vivek Dharkar <ayushidh@cybage.com>
2026-02-18 14:05:07 +0530

    3 tier app 3

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "my-terraform-state-demo-1771308527"
     key            = "envs/prod/terraform.tfstate"
-    region         = "us-east-2"
+    region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
   }
