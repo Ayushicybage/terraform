06f7243bc69840d17f9e1b3c2ce8109195d09953
Ayushi Vivek Dharkar <ayushidh@cybage.com>
2026-02-23 11:32:54 +0530

    3 tier app 5

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -42,4 +42,33 @@ resource "aws_route_table_association" "public_assoc" {
   route_table_id = aws_route_table.public_rt.id
 }
 
+resource "aws_eip" "nat" {
+  vpc  = true
+  tags = { Name = "3tier-nat-eip" }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id   # NAT in the 1st public subnet
+  depends_on    = [aws_internet_gateway.igw]
+  tags          = { Name = "3tier-nat-gw" }
+}
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = { Name = "private-rt" }
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private_rt.id
+}
+
 data "aws_availability_zones" "available" {}
